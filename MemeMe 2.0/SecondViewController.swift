88ecd5b2c297709383a78ba981fa11f23f152d51
Gustavo Trevisan Dini <gustavotrevisandini@gmail.com>
2019-01-21 00:18:15 -0200

    Initial Commit

---
+++ MemeMe 2.0/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MemeMe 2.0
+//
+//  Created by Gustavo Dini on 21/01/19.
+//  Copyright © 2019 Gustavo Dini. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
